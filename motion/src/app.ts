d6c176fcc2ac624bd96ada44902e9791e4a47f3c
jihyeonmun <jhmoon1994@gmail.com>
2021-04-08 19:18:37 +0900

    contents, page function normalized

--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,17 +1,20 @@
-import { ImageComponent } from './components/page/item/image.js';
-import { PageComponent } from "./components/page/page.js";
+
 /*App 클래스가 가장 첫 클래스라고 볼 수 있다*/
 /*페이지 컴포넌트 만들어서 붙여주게 된다.*/
 /*document라는 클래스를 받아와서 보여주게 된다. */ 
+
+import { ImageComponent } from './components/page/item/image.js';
+import { PageComponent } from './components/page/page.js';
+
 class App {
-    private readonly page: PageComponent;
-    constructor(appRoot: HTMLElement) {
-      this.page = new PageComponent();
-      this.page.attachTo(appRoot);
-  
-      const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-      image.attachTo(appRoot, 'beforeend');
-    }
+  private readonly page: PageComponent;
+  constructor(appRoot: HTMLElement) {
+    this.page = new PageComponent();
+    this.page.attachTo(appRoot);
+
+    const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
+    image.attachTo(appRoot, 'beforeend');
   }
-  
-  new App(document.querySelector('.document')! as HTMLElement);
+}
+
+new App(document.querySelector('.document')! as HTMLElement);
--- motion/src/components/page/item/image.ts
+++ motion/src/components/page/item/image.ts
@@ -1,21 +1,17 @@
-export class ImageComponent {
-    private element:HTMLElement;
-    constructor(title: string, url:string){
-        const template = document.createElement('template');
-        template.innerHTML = `<section clsss= "image">
-        <div class="image__holder"><img class =""image__thumbnail"></div>
-        <p class="image__title">${title}</p>
-    </section>`;
-        this.element = template.content.firstElementChild! as HTMLElement;
+import { BaseComponent } from './../../component.js';
 
-        const imageElement = this.element.querySelector('.image__thumbnail')! as HTMLImageElement;
-        imageElement.src = url;
-        imageElement.alt = title;
+export class ImageComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, url: string) {
+    super(`<section class="image">
+            <div class="image__holder"><img class="image__thumbnail"></div>
+            <h2 class="image__title"></h2>
+          </section>`);
 
-        const titleElement = this.element.querySelector('image__title')! as HTMLParagraphElement;
-        titleElement.textContent = title;
-    }
-    attachTo(parent:HTMLElement, position: InsertPosition = 'afterbegin' ){
-        parent.insertAdjacentElement(position, this.element);
-    }
+    const imageElement = this.element.querySelector('.image__thumbnail')! as HTMLImageElement;
+    imageElement.src = url;
+    imageElement.alt = title;
+
+    const titleElement = this.element.querySelector('.image__title')! as HTMLParagraphElement;
+    titleElement.textContent = title;
+  }
 }
