dd4f3a6fc0aabdcc4e9f68fbbe022331308c51e6
jihyeonmun <jhmoon1994@gmail.com>
2021-04-08 21:13:02 +0900

    close function added

--- motion/src/components/component.ts
+++ motion/src/components/component.ts
@@ -3,6 +3,7 @@
  */
 export interface Component { 
     attachTo(parent: HTMLElement, position? : InsertPosition):void;
+    removeFrom(parent: HTMLElement):void;
 }
 
 export class BaseComponent<T extends HTMLElement> implements Component { 
@@ -16,4 +17,12 @@ export class BaseComponent<T extends HTMLElement> implements Component {
    attachTo(parent:HTMLElement, position: InsertPosition = 'afterbegin' ){
        parent.insertAdjacentElement(position, this.element);
     }
+
+    removeFrom(parent:HTMLElement){
+        if(parent !==this.element.parentElement){
+            throw new Error('Parent mismatch!');
+        }
+        parent.removeChild(this.element);
+    }
+
 }
--- motion/src/components/page/page.ts
+++ motion/src/components/page/page.ts
@@ -8,7 +8,13 @@ import { BaseComponent, Component } from './../component.js';
 export interface Composable {
   addChild(child: Component) : void;
 }
+type OnCloseListener =() =>void;
+
+
+
+
 class PageItemComponet extends BaseComponent<HTMLElement> implements Composable{
+  private closeListener ?: OnCloseListener;
   constructor(){
     super(`<li class="page-item">
           <section class="page-item__body"></section>
@@ -16,10 +22,19 @@ class PageItemComponet extends BaseComponent<HTMLElement> implements Composable{
             <button class="close">&times;</button>
           </div>
         </li>`);
+        const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+        closeBtn.onclick = () => {
+          this.closeListener && this.closeListener();
+        };
   }
   addChild(child:Component) {
     const container = this.element.querySelector('.page-item__body')! as HTMLElement;
     child.attachTo(container);
+  }
+
+  setOnCloseListener(listener:OnCloseListener){
+    this.closeListener = listener;
+
   }
 }
 
@@ -33,6 +48,8 @@ export class PageComponent extends BaseComponent<HTMLUListElement> implements Co
     const item = new PageItemComponet();
     item.addChild(section);
     item.attachTo(this.element, 'beforeend');
-
+    item.setOnCloseListener(() => {
+      item.removeFrom(this.element);
+    })
   }
 }
