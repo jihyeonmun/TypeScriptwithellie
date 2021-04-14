b69416e358606d87d5ab8569202bc430989c37b2
Ji Hyeon Moon <jihyeonmoon@Jiui-MacBookPro.local>
2021-04-14 16:00:57 +0900

    dialog, drag function Added

--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,31 +1,84 @@
+
+import { TextSectionInput } from './components/dialog/input/text-input.js';
+import { MediaSectionInput } from './components/dialog/input/media-input.js';
+import { InputDialog, MediaData, TextData } from './components/dialog/dialog.js';
 import { VideoComponent } from './components/page/item/video.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
-import {Composable, PageComponent} from './components/page/page.js';
+import { Composable, PageComponent, PageItemComponent } from './components/page/page.js';
 import { Component } from './components/component.js';
 /*App 클래스가 가장 첫 클래스라고 볼 수 있다*/
 /*페이지 컴포넌트 만들어서 붙여주게 된다.*/
 /*document라는 클래스를 받아와서 보여주게 된다. */ 
 
+type InputComponentConstructor<T = (MediaData | TextData) & Component> = {
+  new (): T;
+};
+
 class App {
   private readonly page: Component & Composable;
-  constructor(appRoot: HTMLElement) {
-    this.page = new PageComponent();
+  constructor(appRoot: HTMLElement, private dialogRoot: HTMLElement) {
+    this.page = new PageComponent(PageItemComponent);
     this.page.attachTo(appRoot);
 
-    const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-    this.page.addChild(image);
+    this.bindElementToDialog<MediaSectionInput>(
+      '#new-image',
+      MediaSectionInput,
+      (input: MediaSectionInput) => new ImageComponent(input.title, input.url)
+    );
 
-    const note = new NoteComponent('Note Title', 'Note Body');
-    this.page.addChild(note);
+    this.bindElementToDialog<MediaSectionInput>(
+      '#new-video',
+      MediaSectionInput,
+      (input: MediaSectionInput) => new VideoComponent(input.title, input.url)
+    );
 
-    const todo = new TodoComponent('Todo Title', 'Todo Item');
-    this.page.addChild(todo);
+    this.bindElementToDialog<TextSectionInput>(
+      '#new-note',
+      TextSectionInput,
+      (input: TextSectionInput) => new NoteComponent(input.title, input.body)
+    );
 
-    const video = new VideoComponent('Video Title', 'https://youtube.com/watch?v=gnnJkTl7bCo');
-    this.page.addChild(video);
+    this.bindElementToDialog<TextSectionInput>(
+      '#new-todo',
+      TextSectionInput,
+      (input: TextSectionInput) => new TodoComponent(input.title, input.body)
+    );
+
+    // For demo :)
+    this.page.addChild(new ImageComponent('Image Title', 'https://picsum.photos/800/400'));
+    this.page.addChild(new VideoComponent('Video Title', 'https://youtu.be/D7cwvvA7cP0'));
+    this.page.addChild(new NoteComponent('Note Title', "Don't forget to code your dream"));
+    this.page.addChild(new TodoComponent('Todo Title', 'TypeScript Course!'));
+    this.page.addChild(new ImageComponent('Image Title', 'https://picsum.photos/800/400'));
+    this.page.addChild(new VideoComponent('Video Title', 'https://youtu.be/D7cwvvA7cP0'));
+    this.page.addChild(new NoteComponent('Note Title', "Don't forget to code your dream"));
+    this.page.addChild(new TodoComponent('Todo Title', 'TypeScript Course!'));
+  }
+
+  private bindElementToDialog<T extends (MediaData | TextData) & Component>(
+    selector: string,
+    InputComponent: InputComponentConstructor<T>,
+    makeSection: (input: T) => Component
+  ) {
+    const element = document.querySelector(selector)! as HTMLButtonElement;
+    element.addEventListener('click', () => {
+      const dialog = new InputDialog();
+      const input = new InputComponent();
+      dialog.addChild(input);
+      dialog.attachTo(this.dialogRoot);
+
+      dialog.setOnCloseListenr(() => {
+        dialog.removeFrom(this.dialogRoot);
+      });
+      dialog.setOnSubmitListenr(() => {
+        const image = makeSection(input);
+        this.page.addChild(image);
+        dialog.removeFrom(this.dialogRoot);
+      });
+    });
   }
 }
 
-new App(document.querySelector('.document')! as HTMLElement);
+new App(document.querySelector('.document')! as HTMLElement, document.body);
